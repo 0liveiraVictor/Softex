ddf5f901d45fa50bc2596615863f57fdee1b847c
Victor Oliveira <106946476+0liveiraVictor@users.noreply.github.com>
2022-10-24 20:30:34 -0300

    Add files via upload

---
+++ Node_JS/DOC---Módulo_02---Projeto_02/main_server.js
@@ -0,0 +1,46 @@
+//importação da biblioteca 'express'
+const express = require("express");
+
+//variável principal do servidor
+const server = express();
+
+//definição da porta de acesso do servidor
+const portAcess = 8080;
+
+//requisição 'http get' com uso de placeholder 'req.params'
+server.get('/rota/:nome/:sobrenome', (request, response)=>{
+
+    const name = request.params.nome;
+    const lastName = request.params.sobrenome;
+
+    //resposta da solicitação realizada pelo cliente
+    response.send("<<< Resposta para a requisição HTTP 'get' solicitada pelo cliente (com uso de placeholder 'req.params') >>>" + `<h1> <<< Seja Bem-Vindo ${name} ${lastName} >>> </h1>`);
+
+})
+
+//requisição 'http get'
+server.get('/rota', (request, response)=>{
+
+    //resposta da solicitação realizada pelo cliente
+    response.send("<<< Resposta para a requisição HTTP 'get' solicitada pelo cliente! >>>");
+
+})
+
+//requisição 'http post'
+server.post('/rota', (request, response)=>{
+
+    //resposta da solicitação realizada pelo cliente
+    response.send("<<< Resposta para a requisição HTTP 'post' solicitada pelo cliente! >>>");
+
+})
+
+//conexão de acesso ao servidor
+server.listen(portAcess, ()=>{
+
+    //impressão no console da execução do servidor
+    console.log(`-------------------------------------------------------------------------------------`); 
+    console.log(`A inicialização do servidor foi executada na porta ${portAcess}.`);
+    console.log(`URL: http://localhost:${portAcess}`);
+    console.log(`-------------------------------------------------------------------------------------`);
+
+});
